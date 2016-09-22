12880625feed33fa96601429dfd8148ea8d9243f
Jaana Burcu Dogan <jbd@google.com>
2016-09-22 15:33:32 -0700

    experimentally bypass []byte to []int

--- device/device.go
+++ device/device.go
@@ -2,7 +2,6 @@ package device
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/mattetti/audio"
 
@@ -17,69 +16,38 @@ const (
 )
 
 type Sink struct {
-	src *al.Source
+	src al.Source
 
-	prevControl int
-	control     int
+	buffer *audio.PCMBuffer
 }
 
 func NewSink(buffer *audio.PCMBuffer) (*Sink, error) {
 	// TODO(jbd): only mono and stereo (8 and 16 bits are allowed)
 	// return error if format is different.
-	if err := al.OpenDevice(); err != nil {
-		return nil, err
-	}
-
+	al.OpenDevice()
 	s := (al.GenSources(1)[0])
 	if code := al.Error(); code != 0 {
 		return nil, fmt.Errorf("device: cannot generate an audio source [err=%x]", code)
 	}
-	sink := &Sink{src: &s}
-	// make sure there is always n buffers in the main source
-	go sink.run(buffer)
-	return sink, nil
-}
-
-func (s *Sink) run(buffer *audio.PCMBuffer) {
-	buf := al.GenBuffers(1)[0]
-	for {
-		src := *s.src
-		if n := src.BuffersProcessed(); n > 0 || src.BuffersQueued() < 1 {
-			f := alFormat(buffer.Format)
-			buf.BufferData(f, buffer.AsBytes(), int32(buffer.Format.SampleRate))
-			src.QueueBuffers(buf)
-			continue
-		}
-		// control playing, pausing and stopping in the main loop.
-		if s.prevControl != s.control {
-			switch s.control {
-			case play:
-				al.PlaySources(src)
-			case pause:
-				al.PauseSources(src)
-			case stop:
-				al.StopSources(src)
-			}
-			s.prevControl = s.control
-		}
-		// TODO(jbd): if EOF, return.
-		time.Sleep(50 * time.Millisecond)
-	}
+	return &Sink{src: s, buffer: buffer}, nil
 }
 
 func (s *Sink) Play() {
-	// TODO(jbd): return error, dont' return until EOF.
-	s.control = play
+	buf := al.GenBuffers(1)[0]
+	f := alFormat(s.buffer.Format)
+	buf.BufferData(f, s.buffer.Bytes, int32(s.buffer.Format.SampleRate))
+	s.src.QueueBuffers(buf)
+	al.PlaySources(s.src)
 }
 
 func (s *Sink) Pause() {
 	// TODO(jbd): return error, dont' return until EOF.
-	s.control = pause
+	al.PauseSources(s.src)
 }
 
 func (s *Sink) Stop() {
 	// TODO(jbd): return error, dont' return until EOF.
-	s.control = stop
+	al.StopSources(s.src)
 }
 
 func alFormat(f *audio.Format) uint32 {
